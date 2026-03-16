dbb386a08876a0b4f9aebe7461f3d4d2d995b0a6
25070725017-sys <25070725017@sithyd.siu.edu.in>
2026-03-16 21:59:25 +0530

    fixed main.jsx build error

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.js'
-import { CartProvider } from './context/CartContext.js'
+import App from './App.jsx'
+import { CartProvider } from './context/CartContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
